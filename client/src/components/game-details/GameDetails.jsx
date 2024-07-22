aac778fcc0e9916433c388bcb39d47dc7ad2007a
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2024-07-22 16:09:24 +0300

    Add comment functionality

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -2,20 +2,41 @@ import { useEffect, useState } from "react";
 
 import gamesAPI from "../../api/games-api";
 import { useParams } from "react-router-dom";
+import commentsApi from "../../api/comments-api";
 
 
 
 export default function GameDetails() {
     const [game, setGame] = useState({});
-    const { gameId } = useParams()
+    const [username, setUsername] = useState('');
+    const [comment, setComment] = useState('');
+    const { gameId } = useParams();
 
     useEffect(() => {
         (async () => {
             const result = await gamesAPI.getOne(gameId);
-            
+
             setGame(result);
         })();
     }, []);
+
+    const commentSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        const newComment = await commentsApi.create(gameId, username, comment);
+
+        setGame(prevState => ({
+            ...prevState,
+            comments: {
+                ...prevState.comments,
+                [newComment._id]: newComment,
+            }
+        }));
+
+        setUsername('');
+        setComment('');
+
+    };
 
     return (
         <section id="game-details">
@@ -29,22 +50,21 @@ export default function GameDetails() {
                     <p className="type">{game.category}</p>
                 </div>
 
-                <p className="text">{game.description}</p>
+                <p className="text">{game.summary}</p>
 
                 {/* <!-- Bonus ( htmlFor Guests and Users ) --> */}
                 <div className="details-comments">
                     <h2>Comments:</h2>
                     <ul>
-                        {/* <!-- list all comments htmlFor current game (If any) --> */}
-                        <li className="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li className="comment">
-                            <p>Content: The best game.</p>
-                        </li>
+                        {Object.keys(game.comments || {}).length > 0
+                            ? Object.values(game.comments).map(comment => (
+                                <li key={comment._id} className="comment">
+                                    <p>{comment.username}: {comment.text}</p>
+                                </li>
+                            ))
+                            : <p className="no-comment">No comments.</p>
+                        }
                     </ul>
-                    {/* <!-- Display paragraph: If there are no games in the database --> */}
-                    <p className="no-comment">No comments.</p>
                 </div>
 
                 {/* <!-- Edit/Delete buttons ( Only htmlFor creator of this game )  --> */}
@@ -58,8 +78,22 @@ export default function GameDetails() {
             {/* <!-- Add Comment ( Only htmlFor logged-in users, which is not creators of the current game ) --> */}
             <article className="create-comment">
                 <label>Add new comment:</label>
-                <form className="form">
-                    <textarea name="comment" placeholder="Comment......"></textarea>
+                <form className="form" onSubmit={commentSubmitHandler}>
+                    <input
+                        type="text"
+                        placeholder="Pesho"
+                        name="username"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                    />
+
+                    <textarea
+                        name="comment"
+                        placeholder="Comment......"
+                        onChange={(e) => setComment(e.target.value)}
+                        value={comment}
+                    ></textarea>
+
                     <input className="btn submit" type="submit" value="Add Comment" />
                 </form>
             </article>
